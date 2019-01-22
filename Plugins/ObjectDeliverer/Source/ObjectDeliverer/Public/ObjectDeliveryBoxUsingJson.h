803f3c8c633b4f53a8b802ce2c576133a30f7246
ayumax <develop@ayumax.net>
2019-01-22 22:40:44 +0900

    made ObjectDeliveryBoxUsingJson
    * made Object to Json
    
    * Creating Json serializer for UObject
    
    * made ObjectDeliveryBoxUsingJson

--- Plugins/ObjectDeliverer/Source/ObjectDeliverer/Public/ObjectDeliveryBoxUsingJson.h
+++ Plugins/ObjectDeliverer/Source/ObjectDeliverer/Public/ObjectDeliveryBoxUsingJson.h
@@ -20,6 +20,24 @@ public:
 
 	virtual void NotifyReceiveBuffer_Implementation(const TArray<uint8>& buffer) override;
 
+	static TSharedPtr<FJsonValue> ObjectJsonCallback(UProperty* Property, const void* Value);
+
 private:
-	TSharedPtr<FJsonObject> CreateJsonObject(const UObject* Obj);
+	static TSharedPtr<FJsonObject> CreateJsonObject(const UObject* Obj);
+	bool JsonObjectToUObject(const TSharedPtr<FJsonObject>& JsonObject, UObject* OutObject);
+	bool JsonObjectToUStruct(const TSharedPtr<FJsonObject>& JsonObject, const UStruct* StructDefinition, void* OutStruct);
+	bool JsonValueToUProperty(const TSharedPtr<FJsonValue>& JsonValue, UProperty* Property, void* OutValue);
+	bool ConvertScalarJsonValueToUPropertyWithContainer(const TSharedPtr<FJsonValue>& JsonValue, UProperty* Property, void* OutValue);
+
+	bool JsonValueToUEnumProperty(const TSharedPtr<FJsonValue>& JsonValue, UEnumProperty* Property, void* OutValue);
+	bool JsonValueToUNumericProperty(const TSharedPtr<FJsonValue>& JsonValue, UNumericProperty *NumericProperty, void* OutValue);
+	bool JsonValueToUBoolProperty(const TSharedPtr<FJsonValue>& JsonValue, UBoolProperty *BoolProperty, void* OutValue);
+	bool JsonValueToUStrProperty(const TSharedPtr<FJsonValue>& JsonValue, UStrProperty *StringProperty, void* OutValue);
+	bool JsonValueToUArrayProperty(const TSharedPtr<FJsonValue>& JsonValue, UArrayProperty *ArrayProperty, void* OutValue);
+	bool JsonValueToUMapProperty(const TSharedPtr<FJsonValue>& JsonValue, UMapProperty* MapProperty, void* OutValue);
+	bool JsonValueToUSetProperty(const TSharedPtr<FJsonValue>& JsonValue, USetProperty* SetProperty, void* OutValue);
+	bool JsonValueToUTextProperty(const TSharedPtr<FJsonValue>& JsonValue, UTextProperty* TextProperty, void* OutValue);
+	bool JsonValueToUStructProperty(const TSharedPtr<FJsonValue>& JsonValue, UStructProperty *StructProperty, void* OutValue);
+	bool JsonValueToUObjectProperty(const TSharedPtr<FJsonValue>& JsonValue, UObjectProperty *ObjectProperty, void* OutValue);
+
 };
