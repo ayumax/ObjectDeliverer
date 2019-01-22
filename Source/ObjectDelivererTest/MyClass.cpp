0be00b3eef6ee48f8bc1fb280af083bdeec44395
ayumax <develop@ayumax.net>
2019-01-23 00:07:09 +0900

    Write minimum information in readmee

--- Source/ObjectDelivererTest/MyClass.cpp
+++ Source/ObjectDelivererTest/MyClass.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyClass.h"
+#include "Utf8StringDeliveryBox.h"
+#include "ObjectDeliveryBoxUsingJson.h"
 #include "ObjectDelivererManager.h"
 
 MyClass::MyClass()
@@ -16,17 +18,62 @@ void MyClass::Start()
 	auto deliverer = NewObject<UObjectDelivererManager>();
 
 	// bind connected event
-	deliverer->Connected.AddDynamic(serverHelper, &MyClass::OnConnect);
+	deliverer->Connected.AddDynamic(this, &MyClass::OnConnect);
 	// bind disconnected event
-	deliverer->Disconnected.AddDynamic(serverHelper, &MyClass::OnDisConnect);
+	deliverer->Disconnected.AddDynamic(this, &MyClass::OnDisConnect);
 	// bind receive event
-	deliverer->ReceiveData.AddDynamic(serverHelper, &MyClass::OnReceive);
+	deliverer->ReceiveData.AddDynamic(this, &MyClass::OnReceive);
 
 	// start deliverer
 	// + protocol : TCP/IP Server
 	// + Data division rule : Header(BodySize) + Body
 	// + Serialization method : Byte Array
 	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpServer(9099), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+
+	// Change of communication protocol
+	// TCP/IP Server
+	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpServer(9099), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+	// TCP/IP Client
+	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpClient("localhost", 9099, true), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+	// UDP Sender
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketSender("localhost", 9099), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+	// UDP Receiver
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketReceiver(9099), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+	// FixedSize
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketReceiver(9099), UPacketRuleFactory::CreatePacketRuleFixedLength(1024));
+
+	// Header(BodySize) + Body
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketReceiver(9099), UPacketRuleFactory::CreatePacketRuleSizeBody(4, ECNBufferEndian::Big));
+
+	// Split by terminal symbol
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketReceiver(9099), UPacketRuleFactory::CreatePacketRuleTerminate({ TEXT('\r'), TEXT('\n') }));
+
+	// Nodivision
+	deliverer->Start(UProtocolFactory::CreateProtocolUdpSocketReceiver(9099), UPacketRuleFactory::CreatePacketRuleNodivision());
+
+
+	// Byte array
+	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpServer(9099), UPacketRuleFactory::CreatePacketRuleSizeBody());
+
+	// UTF-8 string
+	auto deliverybox = NewObject<UUtf8StringDeliveryBox>();
+	deliverybox->Received.AddDynamic(this, &MyClass::OnReceiveString);
+	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpServer(9099), UPacketRuleFactory::CreatePacketRuleSizeBody(), deliverybox);
+
+	deliverybox->Send(TEXT("ABCDEFG"));
+
+	// Object(Json)
+	auto deliverybox = NewObject<UObjectDeliveryBoxUsingJson>();
+	deliverybox->Received.AddDynamic(this, &MyClass::OnReceiveObject);
+	deliverer->Start(UProtocolFactory::CreateProtocolTcpIpServer(9099), UPacketRuleFactory::CreatePacketRuleSizeBody(), deliverybox);
+
+	auto obj = NewObject<SampleObject>();
+	deliverybox->Send(obj);
 }
 
 void MyClass::OnConnect(UObjectDelivererProtocol* ClientSocket)
@@ -45,4 +92,14 @@ void MyClass::OnDisConnect(UObjectDelivererProtocol* ClientSocket)
 void MyClass::OnReceive(UObjectDelivererProtocol* ClientSocket, const TArray<uint8>& Buffer)
 {
 	// received data buffer
+}
+
+void MyClass::OnReceiveString(UObjectDelivererProtocol* ClientSocket, FString ReceivedString)
+{
+	// received data string
+}
+
+void MyClass::OnReceiveObject(UObjectDelivererProtocol* ClientSocket, const UObject* ReceivedObject)
+{
+	// received data object
 }
