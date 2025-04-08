8b9e368f8773e08dcbb2f54a902624e967d458be
Ayuma Kaminosono <develop@ayumax.net>
2025-04-08 16:24:10 +0900

    Update ue5.4 0408 (#82)
    * removed any unnecessary definitions (#79)
    
    * Updated docs (#81)

--- RunAllTest.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "%~dp0\ObjectDelivererTest.uproject" -ExecCmds="Automation RunTests ObjectDeliverer;Quit" -game -unattended -nopause -testexit="Automation Test Queue Empty" -log=outlog.txt
