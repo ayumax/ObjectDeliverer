1de6dd85d89d03704b46b0c84e472f936f70b825
Ayuma Kaminosono <develop@ayumax.net>
2025-04-08 16:05:46 +0900

    removed any unnecessary definitions (#79)

--- Plugins/ObjectDeliverer/Source/ObjectDelivererTests/Public/ObjectDelivererTests.h
+++ Plugins/ObjectDeliverer/Source/ObjectDelivererTests/Public/ObjectDelivererTests.h
@@ -4,30 +4,6 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-// APIマクロの定義
-#if defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable:4668)
-#endif
-
-// モジュールのAPIマクロを定義
-#if PLATFORM_WINDOWS
-	#ifdef OBJECTDELIVERERTESTS_API
-		#undef OBJECTDELIVERERTESTS_API
-	#endif
-	#if OBJECTDELIVERERTESTS_EXPORTS
-		#define OBJECTDELIVERERTESTS_API __declspec(dllexport)
-	#else
-		#define OBJECTDELIVERERTESTS_API __declspec(dllimport)
-	#endif
-#else
-	#define OBJECTDELIVERERTESTS_API
-#endif
-
-#if defined(_MSC_VER)
-#pragma warning(pop)
-#endif
-
 class FObjectDelivererTestsModule : public IModuleInterface
 {
 public:
