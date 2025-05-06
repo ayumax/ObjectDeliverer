6eb80cd754d493cf4d70dbc44b63b56b5b0aa55b
ayumax <develop@ayumax.net>
2025-05-06 20:15:11 +0900

    Add test scripts and remove status badges from README

--- Froola/run_tests_mac.bat
+++ Froola/run_tests_mac.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -l ..\ -t -o .\results -e Mac
+Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -l ..\ -t -o %~dp0results -e Mac
--- Froola/run_tests_win.bat
+++ Froola/run_tests_win.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -l ..\ -t -o C:\repos\ObjectDeliverer\Froola\results -e Windows
+Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -l ..\ -t -o %~dp0results -e Windows
