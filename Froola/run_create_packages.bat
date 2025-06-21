a09303ba8c634bd4d4499242e469863a7ab8c609
ayumax <develop@ayumax.net>
2025-06-21 14:31:22 +0900

    Updated froola settings

--- Froola/run_create_packages.bat
+++ Froola/run_create_packages.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -v 5.4,5.5,5.6 -u git@github.com:ayumax/ObjectDeliverer.git -t -c -o packages --copy-package-after-build
+Froola.exe plugin -n ObjectDeliverer -p ObjectDelivererTest -v 5.4,5.5,5.6 -e Windows,Mac,Linux -u git@github.com:ayumax/ObjectDeliverer.git -t -c -o packages --copy-package-after-build
